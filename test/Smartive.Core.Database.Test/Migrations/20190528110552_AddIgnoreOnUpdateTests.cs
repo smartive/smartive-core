9f1bf669df77eac997d1962b9802c7ed74ab726b
Christoph Bühler <buehler@users.noreply.github.com>
2019-05-28 13:41:28 +0200

    feat: add ignore on update attribute for models (#3)
    * add the attribute
    
    * add tests for ignore on update

---
+++ test/Smartive.Core.Database.Test/Migrations/20190528110552_AddIgnoreOnUpdateTests.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Smartive.Core.Database.Test.Migrations
+{
+    public partial class AddIgnoreOnUpdateTests : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "DontSetOnUpdate",
+                table: "Users",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "SetOnUpdate",
+                table: "Users",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DontSetOnUpdate",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "SetOnUpdate",
+                table: "Users");
+        }
+    }
+}
