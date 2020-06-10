2cb23030f96cb06abdad011d8ea0e4c5f6517999
Christoph Bühler <christoph@smartive.ch>
2020-06-10 11:08:29 +0200

    chore: fix build version nr

--- build/Build.cs
+++ build/Build.cs
@@ -23,8 +23,8 @@ public class Build : NukeBuild
     [Parameter("Configuration to build - Default is 'Debug' (local) or 'Release' (server)")]
     readonly Configuration Configuration = IsLocalBuild ? Configuration.Debug : Configuration.Release;
 
-    [Parameter("Version that is built. Needed for packaging. (format: vx.x.x)")]
-    readonly string Version = GitLab.Instance?.CommitTag;
+    [Parameter("Version that is built. Needed for packaging. (format: x.x.x)")]
+    readonly string Version = GitLab.Instance?.CommitTag?.Substring(1);
 
     readonly string NugetKey = Environment.GetEnvironmentVariable("NUGET_KEY");
 
@@ -89,9 +89,9 @@ public class Build : NukeBuild
                 .EnableNoBuild()
                 .SetConfiguration(Configuration)
                 .SetOutputDirectory(ArtifactsDirectory)
-                .SetVersion(Version?.Substring(1) ?? "0.0.0")
-                .SetFileVersion(Version?.Substring(1) ?? "0.0.0")
-                .SetAssemblyVersion(Version?.Substring(1) ?? "0.0.0")
+                .SetVersion(Version ?? "0.0.0")
+                .SetFileVersion(Version ?? "0.0.0")
+                .SetAssemblyVersion(Version ?? "0.0.0")
                 .CombineWith(SourceProjects, (ss, project) => ss
                     .SetProject(project)), Environment.ProcessorCount);
         });
